1e8b89f3a8636f64e9a578fc15df05ea44a024f9
loblachi <codebasic69@gmail.com>
2024-05-11 23:49:32 +0200

    genUsingJS

---
+++ js.js
@@ -0,0 +1,85 @@
+
+let displayArea = document.querySelector('.products-grid');
+let addCartButton = document.querySelectorAll(".addtocart-btn");
+let addedMessage = document.querySelector(".addedMsg");
+/*
+let prod_db = [{
+    img_url : './Images/products/athletic-cotton-socks-6-pairs.jpg',
+    prod_name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    prod_price_cents : 1090,
+    rating : {
+        stars : './Images/ratings/rating-45.png',
+        ppl_rated : 87
+    }
+},
+{
+    img_url : './Images/products/intermediate-composite-basketball.jpg',
+    prod_name : 'Intermediate Size Basketball',
+    prod_price_cents : 2095,
+    rating : {
+        stars : './Images/ratings/rating-40.png',
+        ppl_rated : 127
+    }   
+},
+{
+    img_url : './Images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    prod_name : 'Adults Plain Cotton t-shirt - 2 pack',
+    prod_price_cents : 799,
+    rating : {
+        stars : './Images/ratings/rating-45.png',
+        ppl_rated : 55
+    }   
+},
+{
+    img_url : './Images/products/bathroom-rug.jpg',
+    prod_name : 'Intermediate Size Basketball',
+    prod_price_cents : 1295,
+    rating : {
+        stars : './Images/ratings/rating-30.png',
+        ppl_rated : 17
+    }   
+}]; */
+
+generateHtml(prod_db);
+
+function generateHtml(arrayDatabase){
+    let combinedHtml = '';
+    let singleHtml = '';
+    arrayDatabase.forEach( (value, index) =>{
+     singleHtml =
+     `<div class="product">
+     <div class="image-div">
+       <img src="${value.image}">
+     </div>
+
+     <div class="prod-desc">
+       <p class="prod-name limit-text-to-2-lines">${value.name}</p>
+       <div class="ratings-div">
+         <img src="./Images/ratings/rating-${value.rating.stars*10}.png">
+         <p>${value.rating.count}</p>
+       </div>
+       <p><b>$${value.priceCents/100}</b></p>
+
+       <select name="quantity-products" id="quantity-products">
+         <option value="1">1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+         <option value="7">7</option>
+         <option value="8">8</option>
+         <option value="9">9</option>
+         <option value="10">10</option>
+       </select>
+     </div>
+     <p class="addedMsg"></p>
+     <div class="addtocart-div" onclick="addToCart()">
+       <button class="addtocart-btn">Add to Cart</button>
+     </div>
+  </div>`
+  combinedHtml += singleHtml;
+    });
+    displayArea.innerHTML = combinedHtml;
+}
+
